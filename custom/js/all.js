c8c999b756a574f0295bff94c14b4481c2e0a9d3
Yasu <blackcat7134@gmail.com>
2025-09-01 12:33:54 +0800

    copyRight

--- custom/js/all.js
+++ custom/js/all.js
@@ -9,18 +9,11 @@ function submitHandler(event) {
   const subject = `Contact - ${firstName} ${lastName}`;
   const body = `Name: ${firstName} ${lastName}\nEmail: ${email}\nComments:\n${comments}`;
 
-  const mailto = `mailto:anyu8242@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  const mailto = `mailto:support@medalliance.com.tw?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 
   const link = document.getElementById("mailTo");
   link.href = mailto;
   link.click();
-
-  // 顯示 Bootstrap Alert
-  // const alertBox = document.getElementById("alertBox");
-  // alertBox.style.display = "block";
-  // setTimeout(() => {
-  //   alertBox.classList.add("show");
-  // }, 10);
 
   // ✅ 自動清空表單
   document.getElementById("contactForm").reset();
